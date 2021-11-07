d7bd1a6f2420aa50b0fc4200ff0fcb5953f8a06b
Miguel Cruz <miguelc4600@gmail.com>
2021-11-07 01:00:57 -0400

    Latest Commit

--- src/go.mod
+++ src/go.mod
@@ -2,4 +2,9 @@ module coinapi
 
 go 1.17
 
-require github.com/jamespearly/loggly v0.0.0-20190206220007-041558ec1c7c
+require (
+	github.com/aws/aws-sdk-go v1.41.19
+	github.com/jamespearly/loggly v0.0.0-20190206220007-041558ec1c7c
+)
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
--- src/main.go
+++ src/main.go
@@ -3,42 +3,89 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/jamespearly/loggly"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 	"time"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/jamespearly/loggly"
 )
 
-func poll() {
+type CoinList struct {
+	Data []struct {
+		Name   string `json:"name"`
+		Symbol string `json:"symbol"`
+		Rank   int    `json:"cmc_rank"`
+		Quote  struct {
+			USD struct {
+				Price              float64 `json:"price"`
+				MarketCap          float64 `json:"market_cap"`
+				MarketCapDominance float64 `json:"market_cap_dominance"`
+			} `json:"USD"`
+		} `json:"quote"`
+	} `json:"data"`
+}
+type DBitem struct {
+	CoinRank   int     `json:"coinRank"`
+	CoinName   string  `json:"coinName"`
+	CoinSymbol string  `json:"coinSymbol"`
+	CoinPrice  float64 `json:"coinPrice"`
+}
+
+func checkEnv() {
+	if len(strings.TrimSpace(os.Getenv("AWS_ACCESS_KEY_ID"))) == 0 {
+		fmt.Println("\nNO AWS KEY ID LOADED")
+	} else {
+		fmt.Println("AWS_ACCESS_KEY_ID: " + os.Getenv("AWS_ACCESS_KEY_ID"))
+
+	}
+
+	if len(strings.TrimSpace(os.Getenv("AWS_SECRET_ACCESS_KEY"))) == 0 {
+		fmt.Println("\nNO AWS SECRET LOADED")
+	} else {
+		fmt.Println("AWS_SECRET_ACCESS_KEY: " + os.Getenv("AWS_SECRET_ACCESS_KEY"))
+
+	}
+
+	if len(strings.TrimSpace(os.Getenv("LOGGLY_TOKEN"))) == 0 {
+		fmt.Println("\nNO LOGGY TOKEN LOADED")
+	} else {
+		fmt.Println("LOGGLY TOKEN: " + os.Getenv("LOGGLY_TOKEN"))
+	}
+
+	if len(strings.TrimSpace(os.Getenv("COINMARKETCAP_API_KEY"))) == 0 {
+		fmt.Println("\nNO COINMARKETCAP_API_KEY LOADED")
+	} else {
+		fmt.Println("COINMARKETCAP_API_KEY: " + os.Getenv("COINMARKETCAP_API_KEY"))
+	}
+}
+
+// Return a populated struct of the top 100 coins from CoinMarketCap
+// while providing logging, printing, and caching of the results
+func fetchCoinAPI() CoinList {
+	// Loading in Enviroment Variables
+	apiKey := os.Getenv("COINMARKETCAP_API_KEY")
 	url := "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"
 
-	coinClient := http.Client{
-		Timeout: time.Second * 2, // Timeout after 2 seconds
-	}
+	//Create a Request to the API
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Get Enviroment Variables For API Keys
-	apiKey := os.Getenv("COINMARKETCAP_API_KEY")
-
-	// Establish Connection to Loggly API
-	client := loggly.New("CoinAPI")
-
-	//Make a request to the api
-	req, err = http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	//Set the api key in the header
+	// Set API Key
 	req.Header.Set("X-CMC_PRO_API_KEY", apiKey)
 
+	// Create a HTTP Client to Execute the Request and generate a response
+	coinClient := http.Client{
+		Timeout: time.Second * 2, // Maximum of 2 secs
+	}
+	// Execute the Request and generate a response
 	res, getErr := coinClient.Do(req)
 	if getErr != nil {
 		log.Fatal(getErr)
@@ -50,25 +97,14 @@ func poll() {
 		log.Fatal(readErr)
 	}
 
-	// Creating a strcut to hold the data from the response body
-	type CoinList struct {
-		Data []struct {
-			Name   string `json:"name"`
-			Symbol string `json:"symbol"`
-			Rank   int    `json:"cmc_rank"`
-			Quote  struct {
-				USD struct {
-					Price              float64 `json:"price"`
-					MarketCap          float64 `json:"market_cap"`
-					MarketCapDominance float64 `json:"market_cap_dominance"`
-				} `json:"USD"`
-			} `json:"quote"`
-		} `json:"data"`
-	}
-
-	// print out the coinlist and sort by rank and display price in USD with reduced precision
+	// Unmarshall the response into a coinList structure
 	coinList := &CoinList{}
 	json.Unmarshal(body, coinList)
+
+	// Establish Connection to Loggly API
+	client := loggly.New("CoinAPI")
+
+	// print out the coinlist and sort by rank and display price in USD with reduced precision
 	for i := 0; i < len(coinList.Data); i++ {
 		fmt.Printf("%d. %s (%s) - %.4f USD\n", coinList.Data[i].Rank, coinList.Data[i].Name, coinList.Data[i].Symbol, coinList.Data[i].Quote.USD.Price)
 		//Store in a string
@@ -78,29 +114,90 @@ func poll() {
 		fmt.Println("err", err)
 	}
 
-	//Create a dynamoDB client
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
-	svc := dynamodb.New(sess)
-
-	
-
-	//serialize the coinlist to json
-	jsonCoinList, _ := json.Marshal(coinList)
-	//write the json to a file
-	err = ioutil.WriteFile("coinlist.json", jsonCoinList, 0644)
-
-	// pretty print the json
+	// Serialize Json and Write the json to a file in PrettyPrint form
 	prettyJSON, _ := json.MarshalIndent(coinList, "", " ")
-	fmt.Println(string(prettyJSON))
+	err = ioutil.WriteFile("coinlist.json", prettyJSON, 0644)
+	fmt.Print(err)
+
+	// Return Fetched CoinList
+	return *coinList
 
 }
 
+func addItemtoDB(item DBitem) {
+	// Start AWS Session (Env variables must be provided before hand)
+	//sess := session.Must(session.NewSessionWithOptions(session.Options{SharedConfigState: session.SharedConfigEnable}))
+	
+	// Start AWS Session in us east 
+	sess := session.Must(session.NewSession(&aws.Config{Region: aws.String("us-east-1")}))
+
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+	_ = svc
+
+	// Process Item Input
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		fmt.Println("Got error marshalling item:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// Add Item to Table
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("mcruz-CoinMarketCap"),
+	}
+
+	_, err = svc.PutItem(input)
+
+	if err != nil {
+		fmt.Println("Got error calling addItemtoDB: ")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	
+	// Map struct contents to string
+	itemString, _ := json.Marshal(item)
+	
+	// Loggly
+	client := loggly.New("CoinAPI")
+	
+	// Send to Loggly
+	fmt.Println("Added Item to DB")
+	err2 := client.EchoSend("info", "Added item to DB:" + string(itemString))
+	fmt.Println("err", err2)
+
+}
+
+func pollAndStore(PollingRateSecs time.Duration) {
+	time.Sleep(PollingRateSecs * time.Second)
+	fmt.Println("Debug: POLLING NOW")
+
+	// Fetch the CoinList
+	coinList := fetchCoinAPI()
+
+	// Loop through the CoinList and add each item to the DB
+	for i := 0; i < len(coinList.Data); i++ {
+		// Create a DBitem
+		item := DBitem{
+			CoinRank:        coinList.Data[i].Rank,
+			CoinName:        coinList.Data[i].Name,
+			CoinSymbol:      coinList.Data[i].Symbol,
+			CoinPrice:       coinList.Data[i].Quote.USD.Price,
+		}
+		// Add to DB
+		addItemtoDB(item)
+	}
+	fmt.Println("Debug: POLLING COMPLETE")
+}
+
 func main() {
-	// poll the api every 15 minutes
+	checkEnv()
 	for {
-		poll()
-		time.Sleep(time.Second * 30)
+		// 15 mins = 900 seconds
+		pollAndStore(5)
 	}
 }
